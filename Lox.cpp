e05dc79f6b7d212570bb0e89bbf2f8edcfb521e3
ddoyediran <ddoyediran@gmail.com>
2024-08-25 13:46:57 +0200

    Base code for scanning the file.

--- Lox.cpp
+++ Lox.cpp
@@ -9,7 +9,12 @@
 
 class Lox {
 
-    private: 
+    //private: 
+    public:
+    Lox() {
+
+    }; 
+
     void runFile(const std::string& path) {
         // Open the file in binary mode
         std::ifstream file(path, std::ios::binary);
@@ -28,34 +33,28 @@ class Lox {
     }
 
     void runPrompt() {
-
+        std::cout << "runPrompt called" << std::endl;
     }
 
     void run(std::string source) {
-        
+        std::cout << "Code is running 'run() function'" << std::endl;
     }
 
 };
 
 
 int main(int argc, char* argv[]) {
-    // std::cout << "You've enter " << argc << " arguments." << std::endl;
-    // // std::cout << "ARGV length: " << argv << "arguments" << std::endl;
-
-    // int i = 0;
-
-    // while (i < argc) {
-    //     std::cout << "Arg " << i + 1 << ": " << argv[i] << std::endl;
-    //     i++;
-    // }
+    Lox lox;
 
     if (argc > 1) {
         std::cout << "Usage: cpplox [scripts]" << std::endl;
         exit(64);
     } else if (argc == 1) {
-        runFile(argv[0]);
+        lox.runFile(argv[0]);
+        //runFile(argv[0]);
     } else {
-        runPrompt();
+        lox.runPrompt();
+        //runPrompt();
     }
 }
 
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# Creating a Programming Interpreter using CPP
+# Creating a Programming language Interpreter using CPP
+
+
+## How to Build & Run the project using CMake
+```bash
+# step 1: Create a 'build' sub-directory. Run the following command from the parent directory
+/loxcpp % mkdir build
+
+# step 2: Change to the 'build' sub-directory.
+loxcpp/build % cd build
+
+# step 3: Run CMake
+loxcpp/build % cmake ..
+
+# step 4: Build the project
+loxcpp/build % make
+
+# step 5: Run the executable directly without any file:
+loxcpp/build % ./loxcpp
+
+# step 6: Run the executable directly with file or passing arguments:
+loxcpp/build % ./loxcpp script.lox
+```
