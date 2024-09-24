37ae58b3b5bfa1de5530ac14849c4b5e8355a28a
ddoyediran <ddoyediran@gmail.com>
2024-09-24 10:11:52 +0200

    Updated generateAst.cpp file with code to implement syntax trees

--- tool/GenerateAst.cpp
+++ tool/GenerateAst.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
+
+class GenerateAst {
+    
+    public:
+    GenerateAst() {}
+
+    void defineAst(const std::string outputDir, const std::string baseName, const std::vector<std::string> types) {
+        std::string path = outputDir + "/" + baseName + ".cpp";
+        std::ofstream writer{path};
+
+        writer << "#pragma once" << std::endl;
+        writer << std::endl;
+
+        writer << "#include <vector>" << std::endl;
+        writer << std::endl;
+
+        writer << "class " << baseName << " {" << std::endl;
+
+        // The AST classes.
+
+
+        writer << "}" << std::endl;
+        writer.close();
+
+    }
+
+    private:
+    
+    
+};
 
 int main(int argc, char* argv[]) {
     if(argc != 2) {
@@ -8,6 +39,15 @@ int main(int argc, char* argv[]) {
         std::exit(64);
     }
 
-    std::string output_dir = argv[1];
+    std::string outputDir = argv[1];
+
+    GenerateAst generateAst;
+
+    generateAst.defineAst(outputDir, "Expr", {
+        "Binary   : Expr left, Token operator, Expr right",
+        "Grouping : Expr expression",
+        "Literal  : Object value",
+        "Unary    : Token operator, Expr right"
+    });
 
 }
